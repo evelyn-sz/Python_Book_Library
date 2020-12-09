4d184bd2c8e3c9425b12d63a46b728a1c5b3beb9
evelyn-sz <evelynszarek@gmail.com>
2020-12-09 20:52:08 +0000

    added new_book form that is working

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
+from models.book import Book
+from models.author import Author
 from repositories import author_repository
 from repositories import book_repository
+
+
 
 from flask import Blueprint
 
@@ -15,14 +20,30 @@ def books():
 @books_blueprint.route('/books/new')
 def new_book():
     authors = author_repository.select_all()
-    return render_template("books/new.html", all_books = authors)
+    return render_template("books/new.html", all_authors = authors)
 
 # CREATE
 # POST '/books'
 
+@books_blueprint.route('/books', methods=['POST'])
+def create_book():
+    title       = request.form['title']
+    genre       = request.form['genre']
+    publisher   = request.form['publisher']
+    author_id   = request.form['author_id']
+
+    author = author_repository.select(author_id)
+    book = Book(title, genre, publisher, author)
+    book_repository.new_book(book)
+    return redirect('/books')
+
 # SHOW
 # GET '/books/<id>'
 
+@books_blueprint.route('/books/<id>', methods=['GET'])
+def show_book(id):
+    book = book_repository.select(id)
+    return render_template('/books/show.html', book=book)
 # EDIT
 # GET '/books/<id>/edit'
 
@@ -32,3 +53,7 @@ def new_book():
 # DELETE
 # DELETE '/books/<id>'
 
+@books_blueprint.route('/books/<id>/delete', methods=['POST'])
+def delete_book(id):
+    book_repository.delete(id)
+    return redirect('/books')
